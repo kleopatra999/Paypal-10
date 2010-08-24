be05ad90e2b918b5433f5733d44ae48dba1f488e
Austin Bischoff <austin.bischoff@gmail.com>
2010-08-24 16:51:31 -0500

    First code revision.  Missing a lot of functionality as well as errors.

---
+++ messages/paypal.php
@@ -0,0 +1,16 @@
+<?php
+return array
+(
+	'10527' => array
+	(
+		'default'  => 'Invalid credit card nummber and type',
+	),
+	'10562' => array
+	(
+		'default'  => 'Invalid credit card expiration year',
+	),
+	'10563' => array
+	(
+		'default'  => 'Invalid credit card expiration month',
+	),
+);
